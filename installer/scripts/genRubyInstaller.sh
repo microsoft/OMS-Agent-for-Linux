5e5285cc3195b67d21209f154037e745c604a4ad
Jeff Coffler <REDMOND\jeffcof>
2015-09-09 14:54:33 -0700

    Add etc optional extension to Ruby package for fluentd

--- installer/scripts/genRubyInstaller.sh
+++ installer/scripts/genRubyInstaller.sh
@@ -2,6 +2,32 @@
 
 set -e
 
+# For running this script, you must be running on a 64-bit system!
+# This is for text substitution that happens below.
+
+if [ `uname -m` != "x86_64" ]; then
+    echo "This script must be run from a 64-bit system" >& 2
+    exit 1
+fi
+
+substitute_arch()
+{
+    if [ -z "$1" ]; then
+	echo "Internal error: substitute_arch requires a parameter" >& 2
+	exit 1
+    fi
+
+    # Substitute machine name as needed
+    echo $1 | grep -q -- "/extensions"
+    if [ $? -eq 0 ]; then
+	RESULT=`echo $1 | sed "s~/x86_64-linux~/\\${{RUBY_ARCH}}~"`
+    else
+	RESULT=`echo $1 | sed "s~/x86_64-linux~/\\${{RUBY_ARCM}}~"`
+    fi
+
+    echo -n "$RESULT"
+}
+
 # This script will generate a "starting point" for an InstallBuilder data file.
 # For example, this generated ruby.data in the omsagent project. This is useful
 # for a package that has an install process (i.e. make install), but we need to
@@ -32,6 +58,7 @@ NEW_BASE_DIR=""
 for i in `find $SOURCE_DIR -name \* -print`; do
     if [ -d $i ]; then
         DIR_NAME=`echo $i | sed "s~$SOURCE_DIR~~"`
+	[ -n "$DIR_NAME" ] && DIR_NAME=`substitute_arch $DIR_NAME`
         STAT_INFO=`stat -c "%a; %U; %G" $i`
         printf "%-55s %s\n" "\${{RUBY_DEST}}${DIR_NAME};" "$STAT_INFO" >> $OUTPUT_DIR
     else
@@ -40,6 +67,7 @@ for i in `find $SOURCE_DIR -name \* -print`; do
         NEW_BASE_DIR=$OLD_BASE_DIR
 
         FILE_NAME=`echo $i | sed "s~$SOURCE_DIR~~"`
+	FILE_NAME=`substitute_arch $FILE_NAME`
         STAT_INFO=`stat -c "%a; %U; %G" $i`
         printf "%-72s %-64s %s\n" "\${{RUBY_DEST}}${FILE_NAME};" "\${{RUBY_INT}}${FILE_NAME};" "$STAT_INFO" >> $OUTPUT_FILE
     fi
