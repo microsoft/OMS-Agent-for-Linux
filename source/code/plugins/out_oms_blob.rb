88ac2a67a7a56d4068f6ec68b949ea2952f8ae22
Robbie Zhang <junjiez@microsoft.com>
2016-04-08 12:36:49 -0700

    Change the header name of the TimeZoneId to all lowercase

--- source/code/plugins/out_oms_blob.rb
+++ source/code/plugins/out_oms_blob.rb
@@ -70,7 +70,7 @@ module Fluent
     def create_blob_put_request(uri, msg)
       headers = {}
 
-      headers["x-ms-meta-TimeZoneId"] = OMS::Common.get_current_timezone
+      headers["x-ms-meta-timezoneid"] = OMS::Common.get_current_timezone
       headers["Content-Type"] = "application/octet-stream"
       headers["Content-Length"] = msg.bytesize.to_s
 
