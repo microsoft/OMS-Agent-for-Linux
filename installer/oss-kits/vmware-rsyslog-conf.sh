61d5ba80087756e96b3adfe85625ad87794dc632
Arjun Urs <arjunursonline@gmail.com>
2016-09-08 13:35:38 -0700

    script updation and test case comments

--- installer/oss-kits/vmware-rsyslog-conf.sh
+++ installer/oss-kits/vmware-rsyslog-conf.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+RestartService() {
+    if [ -z "$1" ]; then
+        echo "RestartService requires parameter (service name to restart)" 1>&2
+        return 1
+    fi
+
+    echo "Restarting service: $1"
+
+    # Does the service exist under systemd?
+    local systemd_dir=$(${{OMS_SERVICE}} find-systemd-dir)
+    pidof systemd 1> /dev/null 2> /dev/null
+    if [ $? -eq 0 -a -f ${systemd_dir}/${1}.service ]; then
+        /bin/systemctl restart $1
+    else
+        if [ -x /usr/sbin/invoke-rc.d ]; then
+            /usr/sbin/invoke-rc.d $1 restart
+        elif [ -x /sbin/service ]; then
+            /sbin/service $1 restart
+        elif [ -x /bin/systemctl ]; then
+            /bin/systemctl restart $1
+        else
+            echo "Unrecognized service controller to start service $1" 1>&2
+        return 1
+        fi
+     fi    
+}
+
 rsyslog_configuration(){
     echo 'Checking for RSyslog installation'
     check_if_rsyslog_is_installed
@@ -10,7 +37,7 @@ check_if_rsyslog_is_installed() {
     then
         add_rsyslog_configuration
     else
-        echo 'RSyslog is not installed.'
+        echo 'RSyslog is not installed.' 1>&2
     fi
 }
 
@@ -20,23 +47,25 @@ add_rsyslog_configuration(){
     log_path="/var/log/vmware"
     log_file="esxi-syslog.log"
 
-    sudo mkdir $log_path
-    sudo touch "$log_path/$log_file"
-    sudo chmod 644 "$log_path/$log_file"
+    mkdir $log_path
+    touch "$log_path/$log_file"
+    chmod 644 "$log_path/$log_file"
+    
     echo 'module(load="imtcp")' > $file
     echo 'input(type="imtcp" port="'$listen_port'")' >> $file
     echo '$template myFormat, "%timestamp:::date-year%-%timestamp:::date-month%-%timestamp:::date-day% %timestamp:::date-hour%:%timestamp:::date-minute%:%timestamp:::date-second% : %timereported:::date-year%-%timereported:::date-month%-%timereported:::date-day% %timereported:::date-hour%:%timereported:::date-minute%:%timereported:::date-second% : %hostname% : %fromhost-ip% : %syslogfacility% : %syslogseverity-text% : %programname% : %msg%\n"' >> $file
     echo '$template PerHostLog,"'$log_path'/'$log_file'"' >> $file
     echo 'if $fromhost-ip != "127.0.0.1"'' then -?PerHostLog;myFormat' >> $file
     echo '& ~' >> $file
+    chown omsagent:omsagent $file
 
     if [ -f $file ];
     then
-        sudo service rsyslog restart
+        RestartService rsyslog
         add_logrotate_configuration $log_path $log_file
         echo 'RSyslog configuration updated.'
     else
-        echo 'RSyslog configuration could not be updated. Try again later with valid permissions'
+        echo 'RSyslog configuration could not be updated. Try again later with valid permissions' 1>&2
     fi
 }
 
@@ -58,7 +87,7 @@ add_logrotate_configuration(){
         logrotate -d $file
         echo 'Logrotate configuration updated.'
     else
-        echo 'Logrotate configuration could not be updated. Try again later with valid permissions'
+        echo 'Logrotate configuration could not be updated. Try again later with valid permissions' 1>&2
     fi
 }
 
