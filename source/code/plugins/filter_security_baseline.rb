8174e90ec937a0f1b7fb7c445f149aa966fb3b3e
Mark Waitser <markwai@microsoft.com>
2016-11-01 13:41:04 +0200

    Create Linux Security Baseline FluentD plugin. Logic:     Periodic (on start and every 24 hours) run omsbaseline(security baseline scanner and assessment tool),     verify and parse output (in JSON format) and transform result to OMS Security Baseline and Security Baseline Summary blobs. Content:     - security_baseline.conf : FluentD configuration file for Security Baseline filter     - filter_security_baseline.rb : FluentD Security Baseline filter implementation     - security_baseline_lib.rb : Main Security Baseline logic implementation     - omsbaseline_x86 : ASM baseline scanner and assessment tool for 32 bit platforms     - omsbaseline_x64 : ASM baseline scanner and assessment tool for 64 bit platforms     - oms_audits.xml : ASM Linux baseline rules configuration file     - security_baseline_lib_test.rb : Security Baseline UTs Additional changes:     - Delay immediate execution on startup for 60 seconds (sleep 60 command)     - Move oms_audits.xml from installer/conf/omsagent.d/ to source/code/plugins/     - Add Security Baseline files to OMS Lunix Agent installation package     - Add RuleId attribute to support content

---
+++ source/code/plugins/filter_security_baseline.rb
@@ -0,0 +1,40 @@
+
+require_relative 'security_baseline_lib'
+require_relative 'oms_common' 
+
+module Fluent
+  class SecurityBaselineFilter < Filter
+
+    Fluent::Plugin.register_filter('filter_security_baseline', self)
+
+    def configure(conf)
+        super
+        # Do the usual configuration here
+        @hostname = OMS::Common.get_hostname or "Unknown host"
+    end
+
+    def start
+        super
+        # This is the first method to be called when it starts running
+        # Use it to allocate resources, etc.
+    end
+
+    def shutdown
+        super
+        # This method is called when Fluentd is shutting down.
+        # Use it to free up resources, etc.
+    end
+
+    def filter(tag, time, record)       
+        # Create Security Baseline and Security Baseline Summary blobs based on omsbaseline tool scan & assessment results
+        security_baseline = OMS::SecurityBaseline.new(@log)
+        security_baseline_blob, security_baseline_summary_blob = security_baseline.transform_and_wrap(record, @hostname, time)
+
+        # Send Security Baseline Summary to FuentD pipeline.
+        # The data is formatted in correct ODS format and no more handling  is required
+        Fluent::Engine.emit("oms.security_baseline_summary", time, security_baseline_summary_blob)
+
+        return security_baseline_blob
+    end # filter
+  end # class
+end # module
