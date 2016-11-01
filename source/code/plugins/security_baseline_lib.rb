3eb256494c0512ebe867e553b5cd872f73abbec4
Mark Waitser <markwai@microsoft.com>
2016-11-01 13:41:04 +0200

    Create Linux Security Baseline FluentD plugin. Logic:     Periodic (on start and every 24 hours) run omsbaseline(security baseline scanner and assessment tool),     verify and parse output (in JSON format) and transform result to OMS Security Baseline and Security Baseline Summary blobs. Content:     - security_baseline.conf : FluentD configuration file for Security Baseline filter     - filter_security_baseline.rb : FluentD Security Baseline filter implementation     - security_baseline_lib.rb : Main Security Baseline logic implementation     - omsbaseline_x86 : ASM baseline scanner and assessment tool for 32 bit platforms     - omsbaseline_x64 : ASM baseline scanner and assessment tool for 64 bit platforms     - oms_audits.xml : ASM Linux baseline rules configuration file     - security_baseline_lib_test.rb : Security Baseline UTs Additional changes:     - Delay immediate execution on startup for 60 seconds (sleep 60 command)     - Move oms_audits.xml from installer/conf/omsagent.d/ to source/code/plugins/     - Add Security Baseline files to OMS Lunix Agent installation package     - Add RuleId attribute to support content

---
+++ source/code/plugins/security_baseline_lib.rb
@@ -0,0 +1,127 @@
+require 'json'
+require 'securerandom' # SecureRandom.uuid 
+
+require_relative 'oms_common'
+
+module OMS
+    class SecurityBaseline
+
+        def initialize(log)
+            @log = log
+        end
+
+        # ------------------------------------------------------
+        def transform_and_wrap(results, host, time)
+            if results.nil?
+                @log.error "Security Baseline Assessment failed; Empty input"
+                return nil, nil
+            end
+
+            if results["results"].nil? 
+                @log.error "Security Baseline Assessment failed; Invalid input:" + results.inspect
+                return nil, nil
+            end
+
+            results["assessment_id"] = SecureRandom.uuid      
+    
+            asm_baseline_results = results["results"]
+            scan_time = results["scan_time"]
+            assessment_id = results["assessment_id"]
+
+            security_baseline_blob = {
+                "DataType"=>"SECURITY_BASELINE_BLOB", 
+                "IPName"=>"Security",
+                "DataItems"=>[
+                ]
+            }
+
+            asm_baseline_results.each do |asm_baseline_result|
+                if asm_baseline_result["result"] == "MISS"
+                    next
+                end
+                
+                oms_baseline_result = transform_asm_2_oms(asm_baseline_result, scan_time, host, assessment_id)	
+                security_baseline_blob["DataItems"].push(oms_baseline_result)
+            end 
+
+            security_baseline_summary_blob = calculate_summary(results, host, time)
+
+            @log.info "Security Baseline Summary: " + security_baseline_summary_blob.inspect
+
+            return security_baseline_blob, security_baseline_summary_blob
+        end # transform_and_wrap
+    
+        # ------------------------------------------------------   
+        def calculate_summary(results, hostname, time)  
+            asm_baseline_results = results["results"]
+            assessment_id = results["assessment_id"]
+            pass_rules = 0;
+            critical_failed_rules = 0;
+            warning_failed_rules = 0;
+            informational_failed_rules = 0;
+            all_failed_rules = 0;
+    
+            asm_baseline_results.each do |asm_baseline_result|
+
+                if asm_baseline_result["result"] == "MISS"
+                    next 
+                end
+                
+                if asm_baseline_result["result"] == "PASS"
+                    pass_rules += 1
+                    next 
+                end                
+
+                all_failed_rules += 1
+
+                case asm_baseline_result["severity"]
+                when "Critical"
+                    critical_failed_rules += 1
+                when "Warning", "Important"
+                    warning_failed_rules += 1             
+                else
+                    informational_failed_rules += 1
+                end                   
+            end 
+
+            all_assessed_rules = all_failed_rules + pass_rules
+            percentage_of_passed_rules = (pass_rules * 100.0 / all_assessed_rules).round
+    
+            security_baseline_summary_blob = {
+                "DataType" => "SECURITY_BASELINE_SUMMARY_BLOB",
+                "IPName" => "Security",
+                "DataItems" => [
+                    {
+                        "Computer" => hostname,
+                        "TotalAssessedRules" => all_assessed_rules,
+                        "CriticalFailedRules" => critical_failed_rules,
+                        "WarningFailedRules" => warning_failed_rules,
+                        "InformationalFailedRules" => informational_failed_rules,
+                        "PercentageOfPassedRules" => percentage_of_passed_rules,
+                        "AssessmentId" => assessment_id,
+                        "OSName" => "Linux"
+                    }
+                ] 
+            }
+
+            return security_baseline_summary_blob
+        end # calculate_summary 
+
+        # ------------------------------------------------------
+        def transform_asm_2_oms(asm_baseline_result, scan_time, host, assessment_id)
+            oms = {
+                "TimeAnalyzed" => scan_time,
+                "Computer" => host,
+                "CceId"=> asm_baseline_result["cceid"],
+                "Severity" => asm_baseline_result["severity"],
+                "Name" => asm_baseline_result["description"],
+                "AnalyzeResult" => asm_baseline_result["result"] == "PASS" ? "Passed" : "Failed",
+                "AssessmentId" => assessment_id,
+                "OSName" => "Linux",
+                "RuleType" => "Command",
+                "RuleId" => asm_baseline_result["ruleId"]
+            }
+            return oms
+        end # transform_asm_2_oms
+    end # class
+end # module
---
+++ test/code/plugins/security_baseline_lib_test.rb
@@ -0,0 +1,86 @@
+require 'test/unit'
+require_relative 'omstestlib'
+require_relative ENV['BASE_DIR'] + '/source/code/plugins/security_baseline_lib'
+
+class BaselineLibTest < Test::Unit::TestCase
+    def setup
+        OMS::SecurityBaseline.log = OMS::MockLog.new
+    end    
+    
+    def test_baseline_with_correct_input
+
+        baseline_results_str = '{ "baseline_id": "OMS.Linux.1", "base_orig_id": "1", "setting_count": 6, "scan_time": "2016-09-21T10:19:00.66205592Z","results": [ {"msid": "2.1","result": "PASS","error_text": "","cceid": "CCE-3522-0","severity": "Important","description": "The nodev option should be enabled for all removable media.","ruleId": "5c7537f2-b90b-44a4-89c9-4fca5fd79ef7"},{"msid": "2.2","result": "FAIL","error_text": "","cceid": "CCE-4275-4","severity": "Warning","description": "The noexec option should be enabled for all removable media.","ruleId": "7976cc38-fddb-4913-9295-4fcac2e641c3"},{"msid": "2.3","result": "MISS","error_text": "","cceid": "CCE-4042-8","severity": "Important","description": "The nosuid option should be enabled for all removable media.","ruleId": "cdc390c9-fb4a-47f6-90a7-4e1bd6d0e9e6"},{"msid": "5","result": "FAIL","error_text": "","cceid": "CCE-4368-7","severity": "Critical","description": "The nodev-nosuid option should be enabled for all NFS mounts.","ruleId": "7ca24433-3c08-4ff5-9fe2-d8e1830c5829"}, {"msid": "7","result": "FAIL","error_text": "No patches needed","cceid": "CCE-XXXXX-1","severity": "Informational","description": "All available package updates should be installed.","ruleId": "5e0a757f-66f0-4233-a857-efee34906f14"},{"msid": "77","result": "FAIL","error_text": "","cceid": "CCE-4308-3","severity": "Important","description": "The rsh-server package should be uninstalled.","ruleId": "b256491f-f804-4c44-bfa4-057dd2f44c30"}] }'
+        baseline_results_json = JSON.parse(baseline_results_str)
+
+        baseline_results_json["assessment_id"] = "3af00be8-44b9-4925-a64a-d5fd3241ddd3"
+        
+        security_baseline_blob, security_baseline_summary_blob = OMS::SecurityBaseline.transform_and_wrap(baseline_results_json, "test_host", "")        
+        
+        assert_equal(security_baseline_blob["DataType"], "SECURITY_BASELINE_BLOB", "Incorrect 'DataType' value") 
+        assert_equal(security_baseline_blob["IPName"], "Security", "Incorrect 'IPName' value")
+        
+        baseline_item_0 = security_baseline_blob["DataItems"][0]
+        assert_equal(baseline_item_0["TimeAnalyzed"], "2016-09-21T10:19:00.66205592Z", "Incorrect 'TimeAnalyzed' value")
+        assert_equal(baseline_item_0["Computer"], "test_host", "Incorrect 'Computer' value")
+        assert_equal(baseline_item_0["CceId"], "CCE-3522-0", "Incorrect 'CceId' value")
+        assert_equal(baseline_item_0["Severity"], "Important", "Incorrect 'Severity' value")
+        assert_equal(baseline_item_0["Name"], "The nodev option should be enabled for all removable media.", "Incorrect 'Name' value")
+        assert_equal(baseline_item_0["AnalyzeResult"], "Passed", "Incorrect 'AnalyzeResult' value")
+        assert_equal(baseline_item_0["RuleId"], "5c7537f2-b90b-44a4-89c9-4fca5fd79ef7", "Incorrect 'RuleId' value")
+        assert_not_equal(baseline_item_0["AssessmentId"], "3af00be8-44b9-4925-a64a-d5fd3241ddd3", "Incorrect 'AssessmentId' value")
+        assert_equal(baseline_item_0["OSName"], "Linux", "Incorrect 'OSName' value")
+        assert_equal(baseline_item_0["RuleType"], "Command", "Incorrect 'RuleType' value")
+        
+        baseline_item_1 = security_baseline_blob["DataItems"][1]
+        assert_equal(baseline_item_1["TimeAnalyzed"], "2016-09-21T10:19:00.66205592Z", "Incorrect 'TimeAnalyzed' value")
+        assert_equal(baseline_item_1["Computer"], "test_host", "Incorrect 'Computer' value")
+        assert_equal(baseline_item_1["CceId"], "CCE-4275-4", "Incorrect 'CceId' value")
+        assert_equal(baseline_item_1["Severity"], "Warning", "Incorrect 'Severity' value")
+        assert_equal(baseline_item_1["Name"], "The noexec option should be enabled for all removable media.", "Incorrect 'Name' value")
+        assert_equal(baseline_item_1["AnalyzeResult"], "Failed", "Incorrect 'AnalyzeResult' value")
+        assert_equal(baseline_item_1["RuleId"], "7976cc38-fddb-4913-9295-4fcac2e641c3", "Incorrect 'RuleId' value")        
+        assert_not_equal(baseline_item_1["AssessmentId"], "3af00be8-44b9-4925-a64a-d5fd3241ddd3", "Incorrect 'AssessmentId' value")
+        assert_equal(baseline_item_1["OSName"], "Linux", "Incorrect 'OSName' value")        
+        assert_equal(baseline_item_1["RuleType"], "Command", "Incorrect 'RuleType' value")
+
+        # Skip rule with MISS result
+        baseline_item_2 = security_baseline_blob["DataItems"][2]
+        assert_equal(baseline_item_2["TimeAnalyzed"], "2016-09-21T10:19:00.66205592Z", "Incorrect 'TimeAnalyzed' value")
+        assert_equal(baseline_item_2["Computer"], "test_host", "Incorrect 'Computer' value")
+        assert_equal(baseline_item_2["CceId"], "CCE-4368-7", "Incorrect 'CceId' value")
+
+        assert_equal(security_baseline_summary_blob["DataType"], "SECURITY_BASELINE_SUMMARY_BLOB", "Incorrect 'DataType' value") 
+        assert_equal(security_baseline_summary_blob["IPName"], "Security", "Incorrect 'IPName' value")
+        
+        baseline_summary_item = security_baseline_summary_blob["DataItems"][0]
+        assert_equal(baseline_summary_item["Computer"], "test_host", "Incorrect 'Computer' value")
+        assert_equal(baseline_summary_item["TotalAssessedRules"], 5, "Incorrect 'TotalAssessedRules' value")
+        assert_equal(baseline_summary_item["CriticalFailedRules"], 1, "Incorrect 'CriticalFailedRules' value")
+        assert_equal(baseline_summary_item["WarningFailedRules"], 2, "Incorrect 'WarningFailedRules' value")
+        assert_equal(baseline_summary_item["InformationalFailedRules"], 1, "Incorrect 'InformationalFailedRules' value")
+        assert_equal(baseline_summary_item["PercentageOfPassedRules"], 20, "Incorrect 'PercentageOfPassedRules' value")
+        assert_not_equal(baseline_summary_item["AssessmentId"], "3af00be8-44b9-4925-a64a-d5fd3241ddd3", "Incorrect 'AssessmentId' value")
+        assert_equal(baseline_summary_item["OSName"], "Linux", "Incorrect 'OSName' value")
+        
+        assert_equal(baseline_item_0["AssessmentId"], baseline_summary_item["AssessmentId"], "Different 'AssessmentId' between baseline and baseline summary")
+    end
+
+    def test_baseline_with_empty_input
+        baseline_results_json = nil
+      
+        security_baseline_blob, security_baseline_summary_blob = OMS::SecurityBaseline.transform_and_wrap(baseline_results_json, "test_host", "")        
+        
+        assert_equal(security_baseline_blob, nil, "Incorrect error case support") 
+        assert_equal(security_baseline_summary_blob, nil, "Incorrect error case support") 
+    end
+
+    def test_baseline_with_bad_input
+        baseline_results_str = '{ "baseline_id": "OMS.Linux.1" }'    
+        baseline_results_json = JSON.parse(baseline_results_str)
+      
+        security_baseline_blob, security_baseline_summary_blob = OMS::SecurityBaseline.transform_and_wrap(baseline_results_json, "test_host", "")        
+        
+        assert_equal(security_baseline_blob, nil, "Incorrect error case support") 
+        assert_equal(security_baseline_summary_blob, nil, "Incorrect error case support") 
+    end
+end
