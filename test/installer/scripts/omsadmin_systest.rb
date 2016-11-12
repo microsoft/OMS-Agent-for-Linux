935a2cf64c51dfff5a718c8cd7c1d9bc903c30ad
Laura Galbraith <lagalbra@microsoft.com>
2016-11-12 10:12:38 -0800

    Refactor maintenance tasks and heartbeat for multihoming and tests

--- test/installer/scripts/omsadmin_systest.rb
+++ test/installer/scripts/omsadmin_systest.rb
@@ -1,80 +1,14 @@
 require 'digest'
+require_relative 'maintenance_systestbase'
 
-class OmsadminTest < Test::Unit::TestCase
+class OmsadminTest < MaintenanceSystemTestBase
   
-  TEST_WORKSPACE_ID=ENV['TEST_WORKSPACE_ID']
-  TEST_SHARED_KEY=ENV['TEST_SHARED_KEY']
-  TEST_WORKSPACE_ID_2=ENV['TEST_WORKSPACE_ID_2']
-  TEST_SHARED_KEY_2=ENV['TEST_SHARED_KEY_2']
-  TEST_PROXY_SETTING=ENV['TEST_PROXY_SETTING']
-
-  def setup
-    @base_dir = ENV['BASE_DIR']
-    @ruby_test_dir = ENV['RUBY_TESTING_DIR']
-    @omsadmin_test_dir = nil
-    @omsadmin_script = nil
-    prep_omsadmin
-  end
-
-  def teardown
-    if @omsadmin_test_dir
-      assert_equal(true, File.directory?(@omsadmin_test_dir))
-      FileUtils.rm_r @omsadmin_test_dir
-      assert_equal(false, File.directory?(@omsadmin_test_dir))
-    end
-  end
-
-  def check_test_keys
-    keys = [TEST_WORKSPACE_ID, TEST_SHARED_KEY, TEST_WORKSPACE_ID_2, TEST_SHARED_KEY_2]
-    keys.each_with_index {|key, index| 
-      assert(key != nil, "Keys[#{index}] should be set by the environment for this test to run.") 
-      assert(key.empty? == false, "Keys[#{index}] should not be empty.")
-    }
-  end
-
-  def prep_omsadmin
-    # Setup test onboarding script and folder
-    prep_omsadmin_script = "#{ENV['BASE_DIR']}/test/installer/scripts/prep_omsadmin.sh"
-    @omsadmin_test_dir = `#{prep_omsadmin_script} #{@base_dir} #{@ruby_test_dir}`.strip()
-    result = $?
-    assert_equal(0, result.to_i, "Unexpected failure setting up the test")
-    assert_equal(true, File.directory?(@omsadmin_test_dir), "'#{@omsadmin_test_dir}' does not exist!")
-
-    @omsadmin_script = "#{@omsadmin_test_dir}/omsadmin.sh"
-    assert_equal(true, File.file?(@omsadmin_script), "'#{@omsadmin_script}' does not exist!")
-    assert_equal(true, File.executable?(@omsadmin_script), "'#{@omsadmin_script}' is not executable.")
-    @proxy_conf = "#{@omsadmin_test_dir}/proxy.conf"
-  end
-
-  def prep_proxy(proxy_setting)
-    proxy_re = /^(https?:\/\/)?(\w+:\w+@)?[^:]+(:\d+)?/
-    assert_match(proxy_re, proxy_setting,
-                 "Proxy setting not in a valid format : [protocol://][user:password@]proxyhost[:port]")
-    assert(@omsadmin_test_dir, "No test directory setup")
-    File.write(@proxy_conf, proxy_setting)
-    assert(File.file?(@proxy_conf), "Proxy conf file missing!")
-  end
-
   def check_cert_perms(path)
     stat = File.stat(path)
     assert_equal(nil, stat.world_readable?, "'#{path}' should not be world readable")
     assert_equal(nil, stat.world_writable?, "'#{path}' should not be world writable")
     assert_equal(true, stat.writable?, "'#{path}' should be writable")
     assert_equal(true, stat.readable?, "'#{path}' should be readable")
-  end
-  
-  def do_onboard(workspace_id, shared_key, should_succeed = true)
-    check_test_keys()
-    onboard_out = `#{@omsadmin_script} -w #{workspace_id} -s #{shared_key}`
-    result = $?
-    if should_succeed
-      assert_equal(0, result.to_i, "Unexpected failure onboarding : '#{onboard_out}'")
-      assert_match(/Onboarding success/, onboard_out, "Did not find onboarding success message")
-    else
-      assert_not_equal(0, result.to_i, "Onboarding was expected to fail but exited with a return code of 0")
-      assert_match(/Error/, onboard_out, "Did not find onboarding error message")
-    end
-    return onboard_out
   end
   
   def post_onboard_validation
@@ -139,12 +73,6 @@ class OmsadminTest < Test::Unit::TestCase
     assert_match(/Error during the onboarding request/, output)
   end
 
-  def get_GUID
-    conf = File.read("#{@omsadmin_test_dir}/omsadmin.conf")
-    guid = conf[/AGENT_GUID=(.*)/, 1]
-    return guid
-  end
-
   def test_reonboard
     do_onboard(TEST_WORKSPACE_ID, TEST_SHARED_KEY)
 
@@ -171,8 +99,10 @@ class OmsadminTest < Test::Unit::TestCase
     output = do_onboard(TEST_WORKSPACE_ID_2, TEST_SHARED_KEY_2)
     new_guid = get_GUID()
 
-    assert_not_equal(old_guid, new_guid, "The GUID should change when reonboarding with a different workspace ID")
-    assert_no_match(/Reusing/, output, "Should not be reusing GUID")
+    # With current multihoming support, the GUID is re-used when onboarding to an additional workspace
+    # TODO Robbie change this when AGENT_GUID logic has been fully converted with multihoming
+    assert_equal(old_guid, new_guid, "The GUID should not change when reonboarding to another workspace ID")
+    assert_match(/Reusing/, output, "Should be reusing GUID")
   end
 
 end
