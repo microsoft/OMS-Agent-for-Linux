8373ca937d731941af6c65586b78ba5ee2095860
duoxu <duoxu@microsoft.com>
2017-01-24 10:46:15 -0800

    Add cluster type field in hdinsight filter
    Read cluster type in hdinsight cluster manifest and append to every
    log/metrics sent to OMS.

--- installer/scripts/hdinsightmanifestreader.rb
+++ installer/scripts/hdinsightmanifestreader.rb
@@ -1,2 +1,9 @@
-result=`sudo python -c "from hdinsight_common.AmbariHelper import AmbariHelper; print AmbariHelper().get_cluster_manifest().deployment.cluster_name"`
-puts result
+cluster_name=`sudo python -c "from hdinsight_common.AmbariHelper import AmbariHelper; print AmbariHelper().get_cluster_manifest().deployment.cluster_name"`
+# old clusters manifest does not have this field
+has_cluster_type=`sudo python -c "from hdinsight_common.AmbariHelper import AmbariHelper; print AmbariHelper().get_cluster_manifest().settings.has_key('cluster_type')"`
+cluster_type = "Unknown cluster type"
+if has_cluster_type.strip == "True"
+   cluster_type = `sudo python -c "from hdinsight_common.AmbariHelper import AmbariHelper; print AmbariHelper().get_cluster_manifest().settings['cluster_type']"`
+end
+results = '{"cluster_name":"'+ cluster_name.strip + '", "cluster_type":"'+ cluster_type.strip + '"}'
+puts results
