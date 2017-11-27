ec00502738dba2f3afacf6003453cf56af6da3cf
Narine Mossikyan <narinem@microsoft.com>
2017-11-27 14:39:51 -0800

    update ruby reference to v2_4_0 and fluentd to 0.12.40

--- source/ext/patches/fluentd/env.rb
+++ source/ext/patches/fluentd/env.rb
@@ -18,6 +18,7 @@ module Fluent
   DEFAULT_CONFIG_PATH = ENV['FLUENT_CONF'] || '/etc/opt/microsoft/omsagent/conf/omsagent.conf'
   DEFAULT_PLUGIN_DIR = ENV['FLUENT_PLUGIN'] || '/opt/microsoft/omsagent/plugin'
   DEFAULT_SOCKET_PATH = ENV['FLUENT_SOCKET'] || '/var/opt/microsoft/omsagent/run/omsagent.sock'
+  DEFAULT_OJ_OPTIONS = {bigdecimal_load: :float, mode: :compat, use_to_json: true}
   DEFAULT_LISTEN_PORT = 25224
   DEFAULT_FILE_PERMISSION = 0640
   DEFAULT_DIR_PERMISSION = 0755
