b0c800a49d05b75004325f2b44e297a216a45b58
Atchut Barli <atchub@microsoft.com>
2018-07-05 11:18:25 -0700

    updating log collector with update

--- tools/LogCollector/source/omslinux_agentlog.py
+++ tools/LogCollector/source/omslinux_agentlog.py
@@ -457,25 +457,60 @@ def copyExtensionFiles():
     return 0
 
 '''
-Copy Update Management Solution logs into /tmp/updatelogs
+Copy Update Management Solution logs into /tmp/omslogs/updateMgmtlogs
 '''
-def copyUpdateFiles(omsLinuxType):
+def copyUpdateFiles():
     cmd='mkdir -p /tmp/omslogs/updateMgmtlogs'
     out=execCommand(cmd)
     writeLogCommand(cmd)
 
     cmd='cp /var/opt/microsoft/omsagent/log/urp.log /tmp/omslogs/updateMgmtlogs'
-    out=execCommand2(cmd)
+    out=execCommand(cmd)
     writeLogCommand(cmd)
 
     cmd='cp /etc/opt/omi/conf/omsconfig/configuration/CompletePackageInventory.xml* /tmp/omslogs/updateMgmtlogs'
     out=execCommand(cmd)
     writeLogCommand(cmd)
     
-    cmd='cp /etc/opt/microsoft/omsagent/run/automationworker/omsupdatemgmt.log /tmp/omslogs'
+    cmd='cp /var/opt/microsoft/omsagent/run/automationworker/*.* /tmp/omslogs/updateMgmtlogs'
+    out=execCommand(cmd)
+    writeLogCommand(cmd)
+
+    cmd='sudo find /var/opt/microsoft/ -name worker.log -exec cp -n {} /tmp/omslogs/updateMgmtlogs \;'
     out=execCommand(cmd)
     writeLogCommand(cmd)
     return 0
+
+'''
+Return the package manager on the system
+'''
+
+def GetPackageManager():
+    # choose default - almost surely one will match.
+    for pkg_mgr in ('apt-get', 'zypper', 'yum'):
+        code = execCommand2('which ' + pkg_mgr)
+        if code is 0:
+            return pkg_mgr                        
+    return None
+
+'''
+obtain the current Available updates on the system
+'''
+
+
+def GetUpdates():
+    mgr = GetPackageManager()
+    if mgr == None:
+        print("Unable to find one of 'apt', 'yum', or 'zypper'.")
+        return None
+    if mgr == 'apt-get':
+        cmd = 'LANG=en_US.UTF8 apt-get -s dist-upgrade | grep "^Inst"'
+    elif mgr == 'yum':
+        cmd = 'sudo yum check-update '
+    elif mgr == 'zypper':
+        cmd = 'zypper -q lu'
+    return cmd
+
 
 '''
 Remove temporary files under /tmp/omslogs once it is archived
@@ -640,6 +675,18 @@ def execCommand2(cmd):
     except subprocess.CalledProcessError as e:
         print(e.returncode)
         return (e.returncode)
+
+'''
+Common logic to run any command and get always output irrespective of return code
+'''
+def execCommand_always_output(cmd):
+    try:
+        out = subprocess.check_output(cmd, shell=True)
+        return out
+    except subprocess.CalledProcessError as e:
+        print(e.returncode)
+        return e.output
+
 
 '''
 Common logic to save command outputs into /tmp/omslogs/omslinux.out
@@ -814,6 +861,7 @@ try:
           copyCommonFiles(linuxType)
           copyExtensionFiles()
           runExtensionCommands()
+          copyUpdateFiles()
        else:
           sys.exit(1)
     elif(omsInstallType == 2):
@@ -822,6 +870,7 @@ try:
        tmpSpace=chkDiskFreeSpace(estSize, 0, cmdSize)
        if(tmpSpace == 0):
           copyCommonFiles(linuxType)
+          copyUpdateFiles()
        else:
           sys.exit(1)
     elif(omsInstallType == 3):
@@ -908,7 +957,21 @@ try:
     out=execCommand(cmd)
     writeLogCommand(cmd)
     writeLogOutput(out)
-        
+
+    '''
+    Run Update Assessment diagnostics commands
+    '''
+    print("Starting to check Available Updates")
+    cmd=GetUpdates()
+    if cmd:
+        out=execCommand_always_output(cmd)
+        writeLogCommand(cmd)
+        writeLogOutput(out)
+    else:
+        writeLogOutput("unknown package manager on the system")
+
+    print("Completed checking Available Updates")
+
     '''
     Logic to capture IOError or OSError in above logic
     '''
@@ -933,4 +996,4 @@ finally:
     compressOMSLog(outDir, compressFile)
     removeTempFiles()
     print('OMS Linux Agent Log is archived in file : %s' % (compressFile))
-    sys.exit()    
+    sys.exit()
