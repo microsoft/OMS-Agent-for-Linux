5f4510f5ac1cf72e7af5caa23d0b40c6b771dd91
shkhisti <32469099+shkhisti@users.noreply.github.com>
2019-01-29 13:35:49 -0800

    fix bug in suto_tail

--- source/code/plugins/in_sudo_tail.rb
+++ source/code/plugins/in_sudo_tail.rb
@@ -99,7 +99,7 @@ module Fluent
     end
  
     def set_system_command
-      @command = "sudo " << RUBY_DIR << TAILSCRIPT << %Q{'@path'} <<  " -p #{@pos_file}"
+      @command = "sudo " << RUBY_DIR << TAILSCRIPT << %Q{'#{@path}'} <<  " -p #{@pos_file}"
     end
 
     def run_periodic
