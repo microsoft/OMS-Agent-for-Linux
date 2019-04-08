0dc323b8196f3c9dd86e6e37fab537981ee5532c
Henry Stolz <hestolz@microsoft.com>
2019-04-08 13:46:44 -0700

    fix common unittest

--- test/code/plugins/oms_common_test.rb
+++ test/code/plugins/oms_common_test.rb
@@ -481,6 +481,7 @@ module OMS
 
     def test_get_logger
       assert_equal(Syslog::LOG_LOCAL0, Common.get_logger(nil).facility, "default log facility should be local0")
+      Syslog.close
       assert_equal(Syslog::LOG_USER, Common.get_logger("user").facility, "correct log facility not parsed")
     end
 
