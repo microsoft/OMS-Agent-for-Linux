6fabdf67456e276136418b3a6579f1fb3d78c341
Henry Stolz <hestolz@microsoft.com>
2019-04-09 11:58:16 -0700

    config test

--- test/code/plugins/oms_configuration_test.rb
+++ test/code/plugins/oms_configuration_test.rb
@@ -135,7 +135,7 @@ module OMS
       assert_not_equal(nil, Configuration.cert, "Certificate should be loaded")
       assert_not_equal(nil, Configuration.key, "Key should be loaded")
       assert_equal(["Azure region value is not set. This must be onpremise machine"], $log.logs, "There was an error loading the configuration")
-      assert_equal("", Configuration.apply_request_intervals(TEST_TOPOLOGY_RESPONSE), "There was an error parsing the request intervals")
+      assert_equal("", Configuration.set_request_intervals(TEST_TOPOLOGY_INTERVAL, TEST_TELEMETRY_INTERVAL), "There was an error parsing the request intervals")
       assert_equal(TEST_TOPOLOGY_INTERVAL, Configuration.topology_interval, "Incorrect topology interval parsed")
       assert_equal(TEST_TELEMETRY_INTERVAL, Configuration.telemetry_interval, "Incorrect telemetry interval parsed")
     end
