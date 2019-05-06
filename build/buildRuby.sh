ab84228ec745242d3e0c58fcd829bd5b7d068434
Abderrahmane Benbachir <abderb@microsoft.com>
2019-05-06 15:19:27 -0700

    fix build failure

--- build/buildRuby.sh
+++ build/buildRuby.sh
@@ -165,7 +165,7 @@ if [ ! -z ${RUBY_CONFIGURE_QUALS_JEMALLOC} ]; then
         ./autogen.sh --prefix=${JEMALLOC_DSTDIR} --libdir=${JEMALLOC_DSTDIR}/lib
         make
         sudo make install_bin install_include install_lib
-        sudo ldconfig
+        elevate ldconfig
     fi
 
 #    export PATH=${JEMALLOC_SRCDIR}/bin:$PATH
