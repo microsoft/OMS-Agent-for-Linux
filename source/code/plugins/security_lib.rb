1e3168eec140cb70992f9b517431365cc500d5ee
Ronmarsiano <ronmarsiano@gmail.com>
2019-07-24 15:09:38 +0300

    Flexible regex

--- source/code/plugins/security_lib.rb
+++ source/code/plugins/security_lib.rb
@@ -5,12 +5,21 @@ module OMS
     require_relative 'omslog'
 
     @@LOG_TYPE_MAPPING = {
-      '%ASA' => 'SECURITY_CISCO_ASA_BLOB',
-      'CEF' => 'SECURITY_CEF_BLOB'
+        '%ASA' => 'SECURITY_CISCO_ASA_BLOB',
+        'CEF' => 'SECURITY_CEF_BLOB'
     }
 
     def self.log_type_mapping
       @@LOG_TYPE_MAPPING
+    end
+
+    def self.get_ident(ident)
+      # To allow a more flexible regex we accept any message containing 'CEF'
+      # or '%ASA' as a valid message and converting it into the correct identifier
+      return 'CEF' if ident.include?('CEF')
+      return '%ASA' if ident.include?('%ASA')
+      OMS::Log.warn_once("Failed to find ident: '#{ident}'")
+      return nil
     end
 
     def self.get_data_type(ident)
