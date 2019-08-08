3599ff0ef27d8e44db8550f5b75a8b82ede9aa6f
Henry Stolz <hestolz@microsoft.com>
2019-08-08 10:34:45 -0700

    bump

--- source/code/plugins/fluent_log_patch.rb
+++ source/code/plugins/fluent_log_patch.rb
@@ -5,46 +5,42 @@
 require_relative 'agent_telemetry_script'
 
 module Fluent
-
   class Log
-
     def event(level, args)
-        time = Time.now
-        message = ''
-        map = {}
-        args.each {|a|
-            if a.is_a?(Hash)
-            a.each_pair {|k,v|
-              map[k.to_s] = v
-            }
-          else
-            message << a.to_s
-          end
-        }
-  
-        map.each_pair {|k,v|
-          message << " #{k}=#{v.inspect}"
-        }
-  
-        unless @threads_exclude_events.include?(Thread.current)
-          record = map.dup
-          record.keys.each {|key|
-            record[key] = record[key].inspect unless record[key].respond_to?(:to_msgpack)
+      time = Time.now
+      message = ''
+      map = {}
+      args.each {|a|
+          if a.is_a?(Hash)
+          a.each_pair {|k,v|
+            map[k.to_s] = v
           }
-          record['message'] = message.dup
-          @engine.push_log_event("#{@tag}.#{level}", time.to_i, record)
-
-          case Log.str_to_level(level.to_s)
-          when LEVEL_ERROR
-            OMS::Telemetry.push_qos_event(OMS::LOG_ERROR, "true", message, OMS::INTERNAL)
-          when LEVEL_FATAL
-            OMS::Telemetry.push_qos_event(OMS::LOG_FATAL, "true", message, OMS::INTERNAL)
-          end
+        else
+          message << a.to_s
         end
-  
-        return time, message
+      }
+
+      map.each_pair {|k,v|
+        message << " #{k}=#{v.inspect}"
+      }
+
+      unless @threads_exclude_events.include?(Thread.current)
+        record = map.dup
+        record.keys.each {|key|
+          record[key] = record[key].inspect unless record[key].respond_to?(:to_msgpack)
+        }
+        record['message'] = message.dup
+        @engine.push_log_event("#{@tag}.#{level}", time.to_i, record)
+
+        case Log.str_to_level(level.to_s)
+        when LEVEL_ERROR
+          OMS::Telemetry.push_qos_event(OMS::LOG_ERROR, "true", message, OMS::INTERNAL)
+        when LEVEL_FATAL
+          OMS::Telemetry.push_qos_event(OMS::LOG_FATAL, "true", message, OMS::INTERNAL)
+        end
+      end
+
+      return time, message
     end
-
   end
-
 end
