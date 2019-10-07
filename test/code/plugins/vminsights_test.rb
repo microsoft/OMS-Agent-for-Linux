bd3f157d6d3e4f802e38ca848af5e117b2864e81
Mike Thompson <mithomps@microsoft.com>
2019-10-07 22:21:36 +0000

    Add new VMInsights plugin
    New plugin for performance and health data gathering.

---
+++ test/code/plugins/vminsights_test.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+require 'test/unit'
+
+require_relative 'in_vminsights_subtest.rb'
+require_relative 'VMInsightsDataCollector_subtest.rb'
+require_relative 'VMInsightsEngine_subtest.rb'
+
