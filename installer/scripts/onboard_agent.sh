50434ca07cd82910ac0f7f7671e493c51a928ab5
Siddhartha Mathiharan <42589365+simathih@users.noreply.github.com>
2019-10-10 16:00:50 -0700

    Update onboard_agent.sh

--- installer/scripts/onboard_agent.sh
+++ installer/scripts/onboard_agent.sh
@@ -6,8 +6,10 @@
 
 
 # Values to be updated upon each new release
-GITHUB_RELEASE="https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.11.0-9/"
-BUNDLE_X64="omsagent-1.11.0-9.universal.x64.sh"
+GITHUB_RELEASE_X86="https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.11.0-9/"
+GITHUB_RELEASE="https://github.com/microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.12.1-0/"
+
+BUNDLE_X64="omsagent-1.12.1-0.universal.x64.sh"
 BUNDLE_X86="omsagent-1.11.0-9.universal.x86.sh"
 
 usage()
@@ -99,5 +101,5 @@ if [ $(uname -m) = 'x86_64' ]; then
     wget -O ${BUNDLE_X64} ${GITHUB_RELEASE}${BUNDLE_X64} && $SUDO sh ./${BUNDLE_X64} ${bundleParameters}
 else
     # x86 architecture
-    wget -O ${BUNDLE_X86} ${GITHUB_RELEASE}${BUNDLE_X86} && $SUDO sh ./${BUNDLE_X86} ${bundleParameters}
+    wget -O ${BUNDLE_X86} ${GITHUB_RELEASE_X86}${BUNDLE_X86} && $SUDO sh ./${BUNDLE_X86} ${bundleParameters}
 fi
