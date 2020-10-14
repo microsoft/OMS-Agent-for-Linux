594e9341d0181181180a317fcdbad738e7df7c72
vishwanath <visnara@microsoft.com>
2020-10-14 15:31:40 -0700

    Hotfix docker version checks

--- installer/oss-kits/docker-oss-test.sh
+++ installer/oss-kits/docker-oss-test.sh
@@ -13,13 +13,18 @@ if [ "$(uname -m)" == "x86_64" ]; then
                 echo "  Neither python2 nor python3 was found, needed to check for Docker version. Docker agent will not be installed." 1>&2
                 exit 1
             fi
-
-            if [ "$($PYTHON -c "print(int(\"$(docker version --format {{.Server.Version}})\".split('.')[0]) >= 17)" 2>/dev/null)" == "True" ]; then
+            # version check (order of below check is important)
+            # supported versions -- major versions >= 17, except azure forks. We need to special case for major version 3, due to Moby azure fork (which did not have 'azure' in them)
+            if [ "$($PYTHON -c "print(\"azure\" in \"$(docker version --format {{.Server.Version}}))" 2>/dev/null)" == "True" ]; then
+                echo "  Azure fork is found in Docker version. Docker agent will not be installed." 1>&2
+                exit 1
+            elif [ "$($PYTHON -c "print(int(\"$(docker version --format {{.Server.Version}})\".split('.')[0]) == 3)" 2>/dev/null)" == "True" ]; then
+                echo "  Docker version 3.* found. Docker agent will not be installed." 1>&2
+                exit 1
+            elif [ "$($PYTHON -c "print(int(\"$(docker version --format {{.Server.Version}})\".split('.')[0]) >= 17)" 2>/dev/null)" == "True" ]; then
                 echo "  Docker version greater or equal than 17.* found. Docker agent will be installed" 1>&2
-            elif [ "$($PYTHON -c "print(int(\"$(docker version --format {{.Server.Version}})\".split('.')[1]) >= 11)" 2>/dev/null)" == "True" ]; then
-                echo "  Docker version greater or equal than 1.11 found. Docker agent will be installed" 1>&2
             else
-                echo "  The installed version of Docker is not supported. Version 1.8 or greater is required. Docker agent will not be installed." 1>&2
+                echo "  The installed version of Docker is not supported. Version 17 or greater is required, and Azure fork is not supported. Docker agent will not be installed." 1>&2
                 exit 1
             fi
         else
