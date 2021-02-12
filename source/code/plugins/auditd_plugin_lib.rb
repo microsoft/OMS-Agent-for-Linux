1c1a1ad8aa3f6946cfc61cc945b04d6a1089a1ed
Tad Glines <taglines@microsoft.com>
2021-02-12 12:13:20 -0800

    Add support for nxOMSAuditdPlugin telemetry (#1283)
    Co-authored-by: Henry Stolz <43051891+hestolz@users.noreply.github.com>

--- source/code/plugins/auditd_plugin_lib.rb
+++ source/code/plugins/auditd_plugin_lib.rb
@@ -1,4 +1,5 @@
 require 'yajl'
+require 'yajl/json_gem'
 require 'securerandom' # SecureRandom.uuid 
 
 require_relative 'oms_common'
@@ -60,6 +61,74 @@ module OMS
             }
 
             return wrapper
+        end
+
+    end # class
+
+    class AuditdDSCLog
+
+        def initialize(log)
+            @log = log
+        end
+
+        def transform(record, hostname, time)
+            operation_name = "#failed#"
+            operation_result = ""
+            dsc_version = ""
+            has_auditd = false
+            auditd_pid = 0
+            auomscollect_pid = 0
+            auoms_pid = 0
+            status_regex = /^STATUS:(?<operation_name>(Test_Start|Test_End|Set_Start|Set_End)):<(?<operation_result>[^>]*)>:(?<version>[^:]*):(?<has_auditd>(true|false)):(?<auditd_pid>[0-9]+):(?<auomscollect_pid>[0-9]+):(?<auoms_pid>[0-9]+)/
+            status_regex.match(record["message"]) { |match|
+                operation_name = match["operation_name"]
+                operation_result = match["operation_result"]
+                dsc_version = match["version"]
+                if match["has_auditd"] == "true"
+                    has_auditd = true
+                end
+                auditd_pid = match["auditd_pid"].to_i
+                auomscollect_pid = match["auomscollect_pid"].to_i
+                auoms_pid = match["auoms_pid"].to_i
+            }
+            if operation_name == "#failed#"
+                @log.error "Failed to parse STATUS message in DSC Log"
+                return {}
+            end
+            dataitem = {}
+            dataitem["Timestamp"] = OMS::Common.format_time(time)
+            dataitem["Computer"] = hostname
+            dataitem["RecordType"] = "AUOMS_METRIC"
+            dataitem["RecordTypeCode"] = 10006
+            dataitem["Namespace"] = "DSC"
+            dataitem["Name"] = operation_name
+            dataitem["version"] = dsc_version
+            dataitem["Message"] = operation_result
+            dataitem["Data"] = {
+                "has_auditd"=>has_auditd,
+                "auditd_pid"=>auditd_pid,
+                "auomscollect_pid"=>auomscollect_pid,
+                "auoms_pid"=>auoms_pid
+            }.to_json
+
+            @log.info "nxOMSAuditdPlugin STATUS: " + dataitem.to_s
+
+            return dataitem
+        end
+
+        def transform_and_wrap(record, hostname, time)
+            if record.is_a?(Hash) && !record.empty? && record.has_key?("message") && record["message"].start_with?("STATUS:")
+                dataitem = transform(record, hostname, time)
+                if !dataitem.empty?
+                    wrapper = {
+                        "DataType"=>"LINUX_AUDITD_BLOB",
+                        "IPName"=>"Security",
+                        "DataItems"=>[dataitem]
+                    }
+                    return wrapper
+                end
+            end
+            return {}
         end
 
     end # class
