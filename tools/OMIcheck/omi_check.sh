ffb07efa0daf7c15dade85b38b7d3e5cbc54cf50
Fikret Can <canfikret@gmail.com>
2021-09-23 10:52:47 -0700

    OMI detect: add VMSS and experimental ARC support (#1378)
    * OMI detect: add VMSS and experimental ARC support
    
    * Incorporate feedback

--- tools/OMIcheck/omi_check.sh
+++ tools/OMIcheck/omi_check.sh
@@ -2,5 +2,5 @@
 
 test -f /opt/omi/bin/omiserver && /opt/omi/bin/omiserver -v
 
-#which rpm > /dev/null && rpm -q omi
-#which dpkg > /dev/null && dpkg -l | grep omi
+which rpm > /dev/null && rpm -qa | grep -E 'omi|omsagent|lad'
+which dpkg > /dev/null && dpkg -l | grep -E 'omi|omsagent|lad'
