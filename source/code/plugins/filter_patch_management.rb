514bbda635d1204846c1f0d9442ae988467dc5a8
Swapnil Jain <65277773+swjai@users.noreply.github.com>
2023-01-06 16:14:58 +0530

    Update filter_patch_management.rb

--- source/code/plugins/filter_patch_management.rb
+++ source/code/plugins/filter_patch_management.rb
@@ -29,6 +29,16 @@ module Fluent
     end
 
     def filter(tag, time, record)
+      begin
+        File.delete("/tmp/az-update-security.list")
+        @log.debug "File deleted successfully"
+      rescue Exception => e
+        if e.errno!=2 #except no such file or directory issue. Errno: ENOENT = 2
+          @log.debug "Caught issues in deleting /tmp/az-update-security.list file. Assessment corrupted."
+          raise e
+        end
+      end
+
       xml_string = record['xml']
       @log.debug "LinuxUpdates : Filtering xml size=#{xml_string.size}"
       linuxUpdates = LinuxUpdates.new(@log)
